b084c6b5fa7a92f42272aeec2e563d7f04b1d958
Andrey Sugonyaev <34348146+oneir0mancer@users.noreply.github.com>
2022-10-06 16:43:21 +0300

    tween sequence

--- Tween/TweenComposition.cs
+++ Tween/TweenComposition.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace Oneiromancer.Tween
 {
@@ -16,6 +17,41 @@ namespace Oneiromancer.Tween
         {
             yield return enumerator;
             yield return nextEnumerator;
+        }
+        
+        /// Consequently run multiple coroutines
+        public static IEnumerator Combine(params IEnumerator[] coroutines)
+        {
+            foreach (var enumerator in coroutines)
+                yield return enumerator;
+        }
+    }
+    
+    public class Sequence
+    {
+        private readonly List<IEnumerator> _coroutines = new List<IEnumerator>();
+
+        public Sequence() {}
+
+        public Sequence(params IEnumerator[] coroutines)
+        {
+            _coroutines.AddRange(coroutines);
+        }
+        
+        public Sequence(IEnumerable<IEnumerator> coroutines)
+        {
+            _coroutines.AddRange(coroutines);
+        }
+
+        public void Add(IEnumerator coroutine)
+        {
+            _coroutines.Add(coroutine);
+        }
+
+        public IEnumerator GetCoroutine()    //TODO track step to pause/unpause
+        {
+            foreach (var routine in _coroutines)
+                yield return routine;
         }
     }
 }
